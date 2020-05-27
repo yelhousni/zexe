7a16a4b38becb6f7247fafcef52c3ebf6b4dde98
Youssef El Housni <youssef.housni21@gmail.com>
2020-05-27 11:37:14 +0200

    clean

--- algebra/src/bw6_761/curves/mod.rs
+++ algebra/src/bw6_761/curves/mod.rs
@@ -52,37 +52,6 @@ impl BW6Parameters for Parameters {
         1574278065184431084,
     ];
     const ATE_IS_LOOP_COUNT_NEG: bool = false;
-    /*
-    const FINAL_EXPONENT_LAST_CHUNK_1: BigInteger768 = BigInteger768([
-        0x3de5800000000089,
-        0x832ba4061000003b,
-        0xc61c554757551c0c,
-        0xc856a0853c9db94c,
-        0x2c77d5ac34cb12ef,
-        0xad1972339049ce76,
-        0x0,
-        0x0,
-        0x0,
-        0x0,
-        0x0,
-        0x0,
-    ]);
-    const FINAL_EXPONENT_LAST_CHUNK_W0_IS_NEG: bool = false;
-    const FINAL_EXPONENT_LAST_CHUNK_ABS_OF_W0: BigInteger768 = BigInteger768([
-        0x6f9440000000008c,
-        0x1aff40fcf0000082,
-        0x9521646d73808c51,
-        0x3ba806d298c79fc5,
-        0xb521a3d9309c6dd0,
-        0x824cd7cfb1e8685a,
-        0xa7f6ef02c228c497,
-        0xa311dc0a5ef6ff10,
-        0x96a147eaf584608d,
-        0x828e2c6f9f4f1494,
-        0x68f6427062e1b0b,
-        0x0,
-    ]);
-    */
     type Fp = Fq;
     type Fp3Params = Fq3Parameters;
     type Fp6Params = Fq6Parameters;
